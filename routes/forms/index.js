b610748e3de32206cadf616685bb8a8cc7b3fe55
cyrilkym <cyril.kym@youengineering.ch>
2023-07-07 14:16:38 +0200

    forms

--- routes/forms/index.js
+++ routes/forms/index.js
@@ -2,29 +2,98 @@ const forms = require('express').Router();
 
 const formsList = [
     {
-        _uid: 1,
-        name: "form 1",
-        value: {
-            fields: [
-                ["name", "text"],
-                ["age", "number"]
-            ]
-        }
+        id: 1,
+        name: "Eventanmeldung",
+        type: "event",
+        fields: [
+            {
+                label: "Name",
+                name: "name",
+                type: "text"
+            },
+            {
+                label: "Vorname",
+                name: "surname",
+                type: "text"
+            },
+            {
+                label: "Strasse",
+                name: "street",
+                type: "text"
+            },
+            {
+                label: "Alter",
+                name: "age",
+                type: "number"
+            },
+            {
+                label: "E-Mail",
+                name: "email",
+                type: "email"
+            },
+        ],
+        submitUrl: "https://storyblok-backend-demo.vercel.app/api/forms"
     },
     {
-        name: "form 2",
-        value: {
-            fields: [
-                ["name", "text"],
-                ["surname", "text"],
-                ["age", "number"]
-            ]
-        }
-    },
+        id: 2,
+        name: "Kontaktanfrage",
+        type: "contact",
+        fields: [
+            {
+                label: "Name",
+                name: "name",
+                type: "text"
+            },
+            {
+                label: "Vorname",
+                name: "surname",
+                type: "text"
+            },
+            {
+                label: "E-Mail",
+                name: "email",
+                type: "email"
+            },
+            {
+                label: "Telefon",
+                name: "phone",
+                type: "text"
+            },
+        ],
+        submitUrl: "https://storyblok-backend-demo.vercel.app/api/forms"
+    }
 ]
 
 forms.get("/", (req, res, next) => {
     res.status(200).json(formsList);
+})
+
+forms.get("/keyvalue", (req, res, next) => {
+    res.status(200).json(formsList.map(f => {
+        return {
+            name: f.name,
+            value: f.id
+        }
+    }));
+})
+
+forms.get("/:id", (req, res) => {
+    const id = Number(req.params.id);
+    let form = null;
+
+    if(req.params.id){
+        formsList.forEach(f =>{
+            if (f.id === id){
+                form = f;
+            }
+        })
+    }
+
+    if(form){
+        res.status(200).json(form);
+    }else{
+        res.status(404).json("kein Formular gefunden")
+    }
 })
 
 module.exports = forms;
