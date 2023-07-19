dd052ca6c5219bce74cace822c5c56f269fefd6b
cyrilkym <cyril.kym@youengineering.ch>
2023-07-19 09:52:41 +0200

    forms

--- routes/products/index.js
+++ routes/products/index.js
@@ -6,12 +6,16 @@ const versicherungen= [
         id: 1,
         name: "Atupri - Basic",
         title: "Schliessen Sie die grössten Versicherungslücken",
-        description: "mit einer günstigen Zusatzversicherung ohne viel Schnickschnack, dafür mit Kostenbeiträgen an Ihr Sportabo, Auslandschutz und Kostenübernahme bei Notfalltransporten. Und das bereits ab CHF 13.50 pro Monat*.",
+        description: "Mit einer günstigen Zusatzversicherung ohne viel Schnickschnack, dafür mit Kostenbeiträgen an Ihr Sportabo, Auslandschutz und Kostenübernahme bei Notfalltransporten. Und das bereits ab CHF 13.50 pro Monat*.",
         price: 13.50,
         hero: {
             url: "https://a.storyblok.com/f/233982/1300x731/8ad6060815/2021_-27_21-023_altb_04_0093_srgb_16-9.jpg",
             texline1: "atupri Basic"
-        }
+        },
+        highlights: [
+            "Sport und Fitness inkl. Online Kurse",
+            "Notfalltransporte und Rettung auch im Ausland"
+        ]
     },
     {
         id: 2,
@@ -20,9 +24,43 @@ const versicherungen= [
         description: "Mit einer Zusatzversicherung schliessen Sie Versicherungslücken, die nicht durch die Grundversicherung gedeckt sind. Damit Sie immer sinnvoll versichert sind und Kosten sparen, erhalten Sie mit der cleveren Zusatzversicherung Mivita neben den allgemeinen Leistungen für jede Lebensphase auch spezifische Leistungen genau dann, wenn Sie diese im Leben brauchen – ganz ohne administrativen Aufwand und Vertragsanpassungen.",
         price: 35.50,
         hero: {
-            url: "https://a.storyblok.com/f/233982/1300x731/3c7ad93211/atupri_wettbewerb_teaserbild_1600x900px.jpg",
+            url: "https://a.storyblok.com/f/233982/1248x420/b5b7b04c49/455-1248x420-grayscale.jpg",
             texline1: "Auf Sie abgestimmt"
-        }
+        },
+        highlights: [
+            "Familien-Rabatt bis zu 50%",
+            "Kombirabatt bis zu 5%"
+        ]
+    },
+    {
+        id: 3,
+        name: "Atupri - Grundversicherung",
+        title: "Die Versicherung für alle",
+        description: "Bei Atupri wählen Sie für die obligatorische Grundversicherung das Modell, das am besten zu Ihnen passt. Ihr bevorzugter Erstkontakt, die Franchise und die Möglichkeiten der Arztwahl bestimmen, wie viel Sie dabei sparen.",
+        price: 250.50,
+        hero: {
+            url: "https://a.storyblok.com/f/233982/1300x731/db358c6c9a/2021_-27_21-023_untr_03_0065_srgb_16-9.jpg",
+            texline1: "für alle das richtige"
+        },
+        highlights: [
+            "Gut und Günstig",
+            "Einfache Handhabung mit myAtupri App"
+        ]
+    },
+    {
+        id: 4,
+        name: "Atupri - Reiseversicherung",
+        title: "Ich plane eine Reise",
+        description: "Unkompliziert, preiswert und schnell: Mit der Atupri Reiseversicherung sind wir auch im Ausland für Sie da. Egal ob Kurz- oder Langaufenthalt, wir haben für Ihre Reise die passende Versicherungslösung parat.",
+        price: 75.30,
+        hero: {
+            url: "https://a.storyblok.com/f/233982/1016x571/7f7ee1bd21/mann-packt-koffer3.gif",
+            texline1: "sorgenfrei reisen"
+        },
+        highlights: [
+            "24/7 für Sie da",
+            "Wir übernehmen die Annulierungskosten"
+        ]
     }
 ]
 
